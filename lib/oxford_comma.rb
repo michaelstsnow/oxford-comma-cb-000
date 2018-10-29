b91c276df1879b3de9eb666ba3b6c9b0fe998983
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 09:32:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,7 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   else
     last_item=array.pop();
-    return array
-    array.join(", ")
+    return array.join(", ")
     #return final_array=array_part_2 + "and #{last_item}"
   end
 end
