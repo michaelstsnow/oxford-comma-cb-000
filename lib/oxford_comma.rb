082afa97db9434b7d0e0f304966c67b6bab1fc42
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 09:34:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  array_size = array.size();
+  if array_size == 1
+    return array[0]
+  elsif array_size ==2
+    return "#{array[0]} and #{array[1]}"
+  else
+    last_item=array.pop();
+    array_2=array.join(", ");
+    final_array=array_2 + ", and #{last_item}";
+    return final_array
+  end
 end
