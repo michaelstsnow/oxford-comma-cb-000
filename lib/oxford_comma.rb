f0bc93d73f5353d90c8d8e248e9f6289f38db139
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 09:24:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array)
   array_size = array.size();
-  last_item=array(array_size-1);
-  array_part_1=array.pop();
-  array_part_2.join(", ")
-  final_array=array_part_2 + "and #{last_item}"
+  if array_size == 1
+    return 
+  else
+    last_item=array(array_size-1);
+    array_part_1=array.pop();
+    array_part_2.join(", ")
+    final_array=array_part_2 + "and #{last_item}"
+  end
 end
