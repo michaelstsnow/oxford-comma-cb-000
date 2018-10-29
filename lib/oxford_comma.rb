db4a71be0a2927b94943e71a56443ba3e4c57719
michaelstsnow <michael.st.snow@gmail.com>
2018-10-29 09:18:53 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,4 @@
 def oxford_comma(array)
   array_size = array.size();
-  array_part_2=[];
-  array.each do { |item|
-    array_part_2 = "#{item}, "
-  }
-  end
-  array_part_3=array_part_2.pop;
-  array_part_3.join
+  array_part_2.join(", ")
 end
